04761757aa406776e74c9bc8552af174a634bc7f
Daniel Javier Pulla S <daniel.javier.pulla@yandex.com>
2023-02-15 19:31:18 -0500

    style(Tailwind): add config files

--- tailwind.config.full.js
+++ tailwind.config.full.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.{html,js,css}"],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
